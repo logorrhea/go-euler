da7cdbfc5db6727d16c078652dc3c09883df8b36
Tyler Funk <tcfunk25@gmail.com>
2014-01-14 15:52:08 -0600

    Did a bit of research on how arrays and slices work in Go.

--- primeFactors.go
+++ primeFactors.go
@@ -6,7 +6,18 @@ import (
 
 func main() {
 
-	// Need a function to determine primality
+	// Example on how to use slices
+	slice1 := []int{1, 2, 3}
+	slice2 := []int{4, 5, 6}
+	fmt.Println(append(slice1, slice2...))
 
-	// Probably could break into smaller bits somehow...
+	// Real code begin!!
+	factors := []int{}
+}
+
+// This should be a recursive method for finding
+// the factors of a number. The endpoints of the function
+// should be the prime factors of the parent number
+func factors(parent int) {
+
 }
