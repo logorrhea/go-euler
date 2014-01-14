9759a90bb9712319aee60b642f09e188e8c13dd1
Tyler Funk <tcfunk25@gmail.com>
2014-01-14 15:16:02 -0600

    Initial commit. Completed the first two problems, created a file for the third.

---
+++ evenFibonacci.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Find the sum of the Fibonnaci numbers whose value does not exceed 4mil
+	total := fib(2, 1, 2)
+	fmt.Printf("Total = %d\n", total)
+}
+
+func fib(total int, prev1 int, prev2 int) int {
+	next := prev1 + prev2
+	if next <= 4000000 {
+		if next%2 == 0 {
+			total += next
+			fmt.Printf("Added %d\n", next)
+		} else {
+			fmt.Printf("Did not add %d\n", next)
+		}
+		return fib(total, prev2, next)
+	} else {
+		return total
+	}
+}
---
+++ primeFactors.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// Need a function to determine primality
+
+	// Probably could break into smaller bits somehow...
+}
